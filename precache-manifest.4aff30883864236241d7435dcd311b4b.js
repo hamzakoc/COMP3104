1d0811b3e81ae5c9e1b534560e27e97f9e7a54c4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-15 12:33:19 +0000

    Deploy hamzakoc/COMP3104 to github.com/hamzakoc/COMP3104.git:gh-pages

---
+++ precache-manifest.4aff30883864236241d7435dcd311b4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c0055e07c1584dded9b",
+    "url": "/COMP3104/static/css/main.2839acd8.chunk.css"
+  },
+  {
+    "revision": "0c0055e07c1584dded9b",
+    "url": "/COMP3104/static/js/main.0c0055e0.chunk.js"
+  },
+  {
+    "revision": "41f235581fd6a198d2ec",
+    "url": "/COMP3104/static/js/runtime~main.41f23558.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "4905593c4893a68ff0360431cf8e5865",
+    "url": "/COMP3104/index.html"
+  }
+];
